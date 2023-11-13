edba7896d9918b28e21dcb2f9ba2d561f8e2de58
NickSettler <moiseevnikita14@gmail.com>
2023-11-13 23:14:04 +0100

    feat(assets): add jetbrains mono font

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
+import './index.scss';
 import App from './App';
 import { Provider } from 'react-redux';
 import { store } from './store/store';
@@ -11,6 +11,7 @@ import { theme } from './utils/theme/theme';
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement,
 );
+
 root.render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
