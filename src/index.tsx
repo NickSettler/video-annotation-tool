cce91637a100af246f6d905e2691eb4895481e87
NickSettler <moiseevnikita14@gmail.com>
2023-12-22 12:08:23 +0100

    feat(utils): add react query client

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.scss';
-import App from './App';
 import { Provider } from 'react-redux';
 import { store } from './store/store';
 import { ModalProvider } from './utils/modal/modal-provider';
 import { ThemeProvider } from '@mui/material';
 import { theme } from './utils/theme/theme';
+import { QueryClientProvider } from '@tanstack/react-query';
+import { client } from './utils/react-query/client';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement,
@@ -16,10 +17,12 @@ root.render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
       <Provider store={store}>
+      <QueryClientProvider client={client}>
         <ModalProvider>
           <App />
         </ModalProvider>
-      </Provider>
-    </ThemeProvider>
-  </React.StrictMode>,
+      </QueryClientProvider>
+    </Provider>
+  </ThemeProvider>,
+  // </React.StrictMode>,
 );
