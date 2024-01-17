0859ad1d35004ac9babf20a8c9c89c032d95f0af
NickSettler <moiseevnikita14@gmail.com>
2024-01-17 01:09:56 +0100

    feat(sentry): add sending release version to sentry from package.json

--- src/index.tsx
+++ src/index.tsx
@@ -18,10 +18,12 @@ import { routes } from './utils/router';
 import { QueryClientProvider } from '@tanstack/react-query';
 import { client } from './utils/react-query/client';
 import * as Sentry from '@sentry/react';
+import pkg from '../package.json';
 
 Sentry.init({
   dsn: process.env.REACT_APP_SENTRY_DSN,
   environment: process.env.NODE_ENV,
+  release: `${pkg.version}`,
   tracePropagationTargets: [
     'localhost',
     /^https:\/\/api\.video\.settler\.tech/,
